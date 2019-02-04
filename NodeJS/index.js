2fab46eedc6dd3e9d82fcaa30f041ddacccaae9d
boudradar <hamza.boudradar@etu.unice.fr>
2019-02-04 23:11:34 +0100

    Docker Project

---
+++ NodeJS/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const { mongoose } = require('./db.js');
+var employeeController = require('./controllers/employeeController.js');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: 'http://localhost:8000' }));
+
+app.listen(8000, '0.0.0.0');
+
+
+app.use('/employees', employeeController);
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-DockerProject
+# Docker Readme
+
+## Images
+
+- Angular
+- Node latest
+- Mongo latest
+
+## Images link
+
+https://drive.google.com/file/d/16AiCE2hMd1XqHly9iEMaMkQtXXehwkUq/view?usp=sharing
+
+## Build
+
+$ docker-compose up --build
+
+## Run
+
+$ docker-compose up
+
+## Authors
+
+Hamza BOUDRADAR
+Mehdi SAAD
